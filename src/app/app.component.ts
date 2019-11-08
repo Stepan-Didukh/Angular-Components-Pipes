f4c8255ab81f4e8edfe8b5bab86be6007b637a05
Stepan <56435489+Stepan-Didukh@users.noreply.github.com>
2019-11-08 15:09:30 +0200

    Angular-Task3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,49 +8,7 @@ import {Component} from '@angular/core';
 export class AppComponent {
   title = 'angular';
 
-  inputValue: string = '';
-  inputCont: string = '';
-  statusRegister: boolean = false;
-  statusLogin: boolean = false;
-
-  Houses = [
-    {
-      Street: 'Doroshenka',
-      Area: 75,
-      Price: 1200000
-    },
-    {
-      Street:'Zelena',
-      Area: 135,
-      Price: 1000000
-    },
-    {
-      Street: 'Prirodna',
-      Area: 75,
-      Price: 900000
-    }
-  ];
-
-  login() {
-    this.statusLogin = !this.statusLogin;
-    this.statusRegister = false;
-  }
-
-  register() {
-    this.statusRegister = !this.statusRegister;
-    this.statusLogin = false
-  }
-
-
-  onInput(ev) {
-    this.inputValue = ev.target.value;
-    console.log(ev.target.value);
-  }
-
-  Search() {
-    this.inputCont = this.inputValue
-
-  }
 
 }
+
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { PownumberPipe } from './pipes/pownumber.pipe';
+import { SearchComponent } from './components/search/search.component';
+import { CreateHouseComponent } from './components/create-house/create-house.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ShowAllComponent } from './components/show-all/show-all.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PownumberPipe,
+    SearchComponent,
+    CreateHouseComponent,
+    LoginComponent,
+    RegisterComponent,
+    ShowAllComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
